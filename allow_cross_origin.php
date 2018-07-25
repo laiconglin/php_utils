f4a66c4fdaead5f13c1032925dcd46d7888730b4
Lai Conglin <5069073+laiconglin@users.noreply.github.com>
2018-07-25 18:18:21 +0800

    Create allow_cross_origin.php

---
+++ allow_cross_origin.php
@@ -0,0 +1,4 @@
+    header('Access-Control-Allow-Origin:http://localhost:8888');
+    header('Access-Control-Allow-Credentials:true');
+    header('Access-Control-Allow-Methods:GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers:Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Requested-With');
